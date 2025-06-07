42ce69587f49e2f2dc357b8083496763e026d422
María <mariamesarojas@gmail.com>
2025-06-06 22:31:13 -0500

    feat: Set basePath and assetPrefix for Rick and Morty application

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/rick-and-morty',
+  assetPrefix: '/rick-and-morty',
 };
 
 export default nextConfig;
