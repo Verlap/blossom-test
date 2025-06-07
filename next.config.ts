30dbbbc1b4e2b1b126df2ac1e5c0bf5e1a4963bd
María <mariamesarojas@gmail.com>
2025-06-06 22:46:44 -0500

    fix: Update next.config.ts and package.json for correct output and start script

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: '/rick-and-morty',
-  assetPrefix: '/rick-and-morty',
+  
 };
 
 export default nextConfig;
